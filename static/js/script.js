430cf3d6aaf62f3e16b9207ab3b3a855026e24b7
jamshid1598 <dovurovjamshid95@gmail.com>
2021-01-08 19:02:05 +0500

    init

--- static/js/script.js
+++ static/js/script.js
@@ -25,6 +25,10 @@ $('.carousel .carousel-item').each(function(){
 
 
 
+window.replainSettings = { id: 'e977c27a-cfa5-4635-be9d-c2db2f6ba9c2' };
+(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
+var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+})('https://widget.replain.cc/dist/client.js');
 
 
 
